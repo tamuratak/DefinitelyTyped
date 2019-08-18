f22abd5db82338efe228c18ce4889ee2b154a45e
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-08-18 09:25:19 +0900

    add detectAll and others at types/chardet. fix the type of detect which does not return any array without opt.

--- types/chardet/index.d.ts
+++ types/chardet/index.d.ts
@@ -18,9 +18,25 @@ export interface Options {
 
 export type Result = Confidence[] | string | null;
 
-export function detect(buf: Buffer, opts?: Options): Result;
+export function detect(buf: Buffer): string | null;
+export function detect(buf: Buffer, opts: Options): Result;
 
 export function detectFile(path: string, cb: (err: any, result: Result) => void): void;
 export function detectFile(path: string, opts: Options, cb: (err: any, result: Result) => void): void;
 
-export function detectFileSync(path: string, opts?: Options): Result;
+export function detectFileSync(path: string): string | null;
+export function detectFileSync(path: string, opts: Options): Result;
+
+export function detectAll(buf: Buffer, opts?: Options): Confidence[];
+
+export function detectFileAll(
+  path: string,
+  cb: (err: any, result: Result) => void,
+): void;
+export function detectFileAll(
+  path: string,
+  opts: Options,
+  cb: (err: any, result: Result) => void,
+): void;
+
+export function detectFileAllSync(path: string, opts?: Options): Confidence[];
